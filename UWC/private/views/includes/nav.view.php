d20f67530210411cfe6aa5cdeb264acc0c5bf9d1
VDTHP <dangcapnhat664@gmail.com>
2022-11-21 22:39:34 +0700

    nav bar refurbished 2

--- UWC/private/views/includes/nav.view.php
+++ UWC/private/views/includes/nav.view.php
@@ -1,19 +1,22 @@
 <nav class="navbar navbar-light bg-light">
-
-  <div class="justify-content-left">
+  <div class="">
     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">
       <span class="navbar-toggler-icon"></span>
     </button>
-    
+
   </div>
-  <a class="navbar-brand align-items-center" href="index">
-        <img src="../public/assets/img/logo-name.jpg" width="50" height="50" class="d-inline-block align-top" alt="">   
-  </a>
+  <div class="mx-auto order-0">
+  <a class="navbar-brand" href="index">
+        <img src="../public/assets/img/logo.png" width="50" height="50" class="d-inline-block align-top" alt="">   
+      </a>
+  </div>
+  
+  
     
   <!-- Side Nav Bar -->
   <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
   <div class="offcanvas-header">
-    <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Backdroped with scrolling</h5>
+    <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">UWC 2.0</h5>
     <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
   </div>
   <div class="offcanvas-body">
