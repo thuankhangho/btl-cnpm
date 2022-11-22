90df7de6917ff3665aec4200ce2979592fd4c2de
VDTHP <dangcapnhat664@gmail.com>
2022-11-22 22:11:23 +0700

    dashboard (WIP)

---
+++ UWC/private/views/dashboard.view.php
@@ -0,0 +1,79 @@
+<?php $this->view('includes/header') ?>
+<?php $this->view('includes/nav') ?>
+
+<div class="my-container" style="padding: 20px;">
+    <div>
+        <h3>Admin Dashboard</h3>
+    </div>
+    <div class="boxes">
+        <div class="row">
+            <div class="col-sm-3 rounded">
+                <div class="well bg bg-danger rounded">
+                    1st Column
+                </div>
+            </div>
+            <div class="col-sm-3 rounded">
+                <div class="well bg bg-warning rounded">
+                    2nd Column
+                </div>
+            </div>
+            <div class="col-sm-3 rounded">
+                <div class="well bg bg-secondary rounded">
+                    3rd Column
+                </div>
+            </div>
+            <div class="col-sm-3 rounded">
+                <div class="well bg bg-danger rounded">
+                    1st Column
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-2">
+        <div class="row">
+            <div class="container-notif col-6">
+                <table class="table table-hover">
+                    <thead>
+                    <tr>
+                    <th scope="col">Notifications</th>
+                    </tr>
+                    </thead>
+                <tbody>
+                    <tr>
+                    <th scope="row">1</th>
+                    </tr>
+                    <tr>
+                    <th scope="row">2</th>
+                    </tr>
+                    <tr>
+                    <th scope="row">3</th>
+                    </tr>
+                </tbody>
+            </table>
+            </div>
+            <div class="daily-task col-6">
+            <table class="table table-hover">
+                    <thead>
+                    <tr>
+                    <th scope="col">Notifications</th>
+                    </tr>
+                    </thead>
+                <tbody>
+                    <tr>
+                    <th scope="row">1</th>
+                    </tr>
+                    <tr>
+                    <th scope="row">2</th>
+                    </tr>
+                    <tr>
+                    <th scope="row">3</th>
+                    </tr>
+                </tbody>
+            </table>
+            </div>
+        </div>
+        
+    </div>
+    
+    </div>
+</div>
