6408e8f63d591f60389fd302ac43d750d353bc76
thuankhangho <hothuankhang@gmail.com>
2022-12-06 22:17:21 +0700

    Update employees.view.php

--- UWC/private/views/employees.view.php
+++ UWC/private/views/employees.view.php
@@ -15,14 +15,12 @@
 <body>
 
 <h3 class="h-100 d-flex align-items-center justify-content-center">Employee Management</h3>
-<div class="container-fluid p-4 shadow mx-auto" style="max-width: 1300px;">
-
-  <a href="signup">            
+<div class="container-fluid p-4 shadow mx-auto">
+<h4>Collectors</h4>
+  <a href="#">
     <button class="btn btn-sm btn-primary" style="background-color: green"><i class="fa fa-plus"></i> Add New</button>
-  </a>          
-  <h4>Collector</h4><br>
+  </a>
   <div class="card-group justify-content-center">
-                    
     <?php if(isset($data['collectors'])):?>
       <?php foreach ($data['collectors'] as $col):?>
         <div class="card m-2 shadow-sm" style="max-width: 12rem;min-width: 12rem;">
@@ -45,10 +43,13 @@
       </div>
     <?php else:?>
       <h4>No collector were registered</h4>
-    <?php endif;?>
-                          
+    <?php endif;?>                  
   </div>
-  <h4>Janitor</h4><br>
+  
+  <h4>Janitors</h4>
+  <a href="#">            
+    <button class="btn btn-sm btn-primary" style="background-color: green"><i class="fa fa-plus"></i> Add New</button>
+  </a>
   <div class="card-group justify-content-center">
     
     <?php if(isset($data['janitors'])):?>
