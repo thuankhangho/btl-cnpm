d749d6309130ad81f6200c3799af4139fd195b16
TitaniumBull224 <titaniumyama224@gmail.com>
2022-12-06 16:34:06 +0700

    Update Employees.php

--- UWC/private/controllers/Employees.php
+++ UWC/private/controllers/Employees.php
@@ -1,10 +1,11 @@
 <?php
 
 class Employees extends Controller {
-  function index($id = null) {
+  function index2($id = null) {
     $user = new Users();
     $collectors = $user->where('occupation', 'Collector');
     $janitors = $user->where('occupation', 'Janitor');
+
     if ($id) {
       $this->view('employees', ['employee'=>$user->where('id', $id)]);
     } else {
@@ -15,6 +16,49 @@ class Employees extends Controller {
   function tarot($num = 0) {
     $major = array("The Fool", "The Magician", "The High Priestess", "The Emperess", "Emperor", "The Hierophant", "The Lovers", "The Chariot", "Stregth", "The Hermit", "Wheel of Fortune", "Justice", "The Hanged Man", "Death", "Temperance", "The Devil", "The Tower", "The Star", "The Moon", "The Sun", "Judgement", "The World");
     if ($num >= 0 && $num <= 21) echo "Here is your card: ".$major[$num]."\n";
+  }
+  function index($id = null) {
+    $col1 = array(
+      'id'=>9,
+      'username'=>"foursome",
+      'fullname'=>"Yasaka"
+    );
+    $jan1 = array(
+      'id'=>20,
+      'username'=>"threesome",
+      'fullname'=>"Aokiji"
+    );
+    // $col1['id'] = 9;
+    // $col1['username'] = "foursome";
+    // $col1['password'] = "kizaru";
+    // $col1['fullname'] = "Yasaka";
+    // $col1['gender'] = 0;
+    // $col1['birthday'] = "2002-01-02";
+    // $col1['occupation'] = "Collector";
+    // $col1['email'] = "yellowmonkey@yahoo.com";
+    // $col1['phone'] = "014645684";
+    // $col1['address'] = "g5468785hwh";
+
+    // $jan1['id'] = 20;
+    // $jan1['username'] = "threesome";
+    // $jan1['password'] = "Aokiji";
+    // $jan1['fullname'] = "Murakumo";
+    // $jan1['gender'] = 1;
+    // $jan1['birthday'] = "2002-04-14";
+    // $jan1['occupation'] = "Janlector";
+    // $jan1['email'] = "bluebird@yahoo.com";
+    // $jan1['phone'] = "14684132";
+    // $jan1['address'] = "agahdkzuhv";
+
+    $collectors = array($col1, $jan1);
+    $janitors = array($jan1, $col1);
+    if ($id == 9) {
+      $this->view('employees', ['employee'=>$col1]);
+    } elseif ($id == 20) {
+      $this->view('employees', ['employee'=>$jan1]);
+    } else {
+      $this->view('employees', ['collectors'=>$collectors, 'janitors'=>$janitors]);
+    }
   }
 }
 
