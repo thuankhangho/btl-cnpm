a1ca98f73f8cee5500d8b318821bf4549c810ab5
TitaniumBull224 <titaniumyama224@gmail.com>
2022-12-07 10:36:57 +0700

    Update header.view.php

--- UWC/private/views/includes/header.view.php
+++ UWC/private/views/includes/header.view.php
@@ -10,6 +10,11 @@
   <script src="https://kit.fontawesome.com/aba198ab3f.js" crossorigin="anonymous"></script>
   <!-- CSS -->
   <link rel="stylesheet" href="<?=ROOT?>/assets/css/styles.css">
+  <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@700&display=swap" rel="stylesheet">
   <!-- Box Icon-->
   <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
   <!-- <title>Home</title> -->
